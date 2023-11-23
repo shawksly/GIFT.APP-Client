174278aafda40ae703273e6001b0add23c8a9612
shawksly <shawksly@gmail.com>
2023-11-22 21:47:19 -0700

    created login functionality and worked on styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 // import logo from './logo.svg';
 // import './App.css';
+import { Flex, Container } from '@radix-ui/themes';
+import Auth from './components/auth/Auth'
 
 function App() {
+
+  function initializeUser() {
+
+  }
+
+  function updateUser() {
+
+  }
+
+  function clearUser() {
+
+  }
+
   return (
-    <div className="App">
-       <h1 class ='text-3xl bg-color-purple-500'> Hello WORlD! </h1>
-    </div>
+    <Flex
+      width="100%"
+      height="100%"
+      direction="column"
+      align="center"
+      justify="center"
+      className='min-h-screen'
+    >
+      <img src='https://cdn.imgchest.com/files/e4gdcl2w3p4.png' className="h-1/2 p-4">
+      </img>
+      <Container
+        className={`${true ? "class1" : "class2"} h-auth w-screen grow`}
+        // style={{height: var(--auth-height)}}
+      >
+        <h1 className ='text-3xl bg-slate-900 text-white text-center'>GIFT.ME</h1>
+        <Auth />
+      </Container>
+    </Flex>
   );
 }
 
