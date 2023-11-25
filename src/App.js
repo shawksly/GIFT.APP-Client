1e862a27b9385ea09f00b62be24f3f0c62d489e1
SS-93 <Sesughtor@gmail.com>
2023-11-24 22:56:38 -0800

    Signup and Login Route working

--- src/App.js
+++ src/App.js
@@ -6,8 +6,41 @@ import Signup from './components/auth/signup/Signup';
 import {BrowserRouter, Routes, Route, Link, Navigate} from 'react-router-dom'
 import Login from './components/auth/login/Login';
 import Home from './components/home/Home'
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [token, setToken] = useState('');
+
+  // Initialize the isLoggedIn state as false (user is not logged in)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(initializeToken, []);
+
+  function initializeToken() {
+    setToken(localStorage.token);
+    console.log('TOKEN:', localStorage.token);
+
+    // Set the isLoggedIn state based on the presence of the token
+    setIsLoggedIn(!!localStorage.token);
+  }
+
+
+  function updateToken(newToken) {
+    setToken(newToken);
+    localStorage.token = newToken;
+
+    // Set the isLoggedIn state to true when a token is updated (user is logged in)
+    setIsLoggedIn(true);
+  }
+
+  function clearToken() {
+    setToken(''); // Clear the token when logging out
+    localStorage.removeItem('token'); // Remove the token from local storage
+
+    // Set the isLoggedIn state to false when the user logs out
+    setIsLoggedIn(false);
+  }
 
   function initializeUser() {
 
@@ -30,9 +63,9 @@ function App() {
     <BrowserRouter>
    
 <Routes>
-<Route path='/' element = {<Home/>} />
-<Route path='Signup' element = {<Signup/>} />
-<Route path='Login' element = {<Login/>} />
+<Route path='/' element = {<Home loggedIn={isLoggedIn} token = {token}  />} />
+<Route path='Signup' element = {<Signup setToken = {updateToken}  />} />
+<Route path='Login' element = {<Login setToken = {updateToken}/>} />
 </Routes>
 
 
--- src/components/auth/signup/Signup.jsx
+++ src/components/auth/signup/Signup.jsx
@@ -1,101 +1,119 @@
-import {React, useState} from 'react'
+import { React, useState } from "react";
 
-import { useNavigate } from 'react-router'
-import Login from '../login/Login'
+import { useNavigate } from "react-router";
+import PropTypes from "prop-types";
+import Login from "../login/Login";
 
 function Signup(props) {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [userName, setUserName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const signupRoute = "http://localhost:4000/user/signup";
 
-  const signupRoute = 'http://localhost:4000/user/signup';
+  const navigate = useNavigate();
 
-  const navigate = useNavigate ()
-
-  async function SignupInput (e) {
-
-    e.preventDefault ()
-    console.log('testing this function');
+  async function SignupInput(e) {
+    e.preventDefault();
+    console.log("testing this function");
     console.log(userName);
     console.log(email);
     console.log(password);
 
     try {
-      let response = await fetch (signupRoute,{
-
+      let response = await fetch(signupRoute, {
         headers: new Headers({
-'content-type': 'application/json',
-
-
+          "content-type": "application/json",
         }),
 
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify({
-        userName: userName,
-        email: email,
-        password: password
-      
-      })
-
+          userName: userName,
+          email: email,
+          password: password,
+        }),
       });
 
+      console.log(response);
       let results = await response.json();
-      // props.setToken(results.token)
-      
+      props.setToken(results.token);
+
       if (response.status === 200) {
-
-        navigate('/Home')
+        navigate("/Home");
+      } else {
+        console.log("Signup Missed");
       }
-
-else {console.log ('Signup Missed')}
-      
-    } catch (error) {
-      console.log(error)
-    }
-
-
+    } catch (error) {}
   }
 
   return (
-
-    <div> 
-     
-    <div className='flex flex-col items-center justify-center h-screen bg-slate-500 '>
-
-    <div className = 'text-purple-100 font-mono text-3xl'>
-
-<h1> GIFT.ME </h1>
-</div>
-        <div className='w-full max-w-md  bg-slate-700  rounded-xl shadow-md py-8 px-8'> 
-        <h2 className=' text-[28px] font-bold text-purple-100 mb text-center  ' > Sign Up </h2>
-        <form onSubmit = {SignupInput} className='flex flex-col'> 
-        <div className='flex space-x-4 mb-4'>
-<input onChange={e => setUserName(e.target.value) }  placeholder='Username' className=' bg-slate-800 text-white border-0 rounded-md p-2 w-1/2 focus:bg-slate-700
-focus:outline-blue-200 transition ease-in-out duration-150 place-content-baseline placeholder-gray-300' type= 'text' />
-{/* <input placeholder='Last Name' className=' bg-slate-600 text-white border-0 rounded-md p-2 w-1/2 focus:bg-slate-500
+    <div>
+      <div className="flex flex-col items-center justify-center h-screen bg-slate-500 ">
+        <div className="text-purple-100 font-mono text-3xl">
+          <h1> GIFT.ME </h1>
+        </div>
+        <div className="w-full max-w-md  bg-slate-700  rounded-xl shadow-md py-8 px-8">
+          <h2 className=" text-[28px] font-bold text-purple-100 mb text-center  ">
+            {" "}
+            Sign Up{" "}
+          </h2>
+          <form onSubmit={SignupInput} className="flex flex-col">
+            <div className="flex space-x-4 mb-4">
+              <input
+                onChange={(e) => setUserName(e.target.value)}
+                placeholder="Username"
+                className=" bg-slate-800 text-white border-0 rounded-md p-2 w-1/2 focus:bg-slate-700
+focus:outline-blue-200 transition ease-in-out duration-150 place-content-baseline placeholder-gray-300"
+                type="text"
+              />
+              {/* <input placeholder='Last Name' className=' bg-slate-600 text-white border-0 rounded-md p-2 w-1/2 focus:bg-slate-500
 focus:outline-blue-200 transition ease-in-out duration-150 place-content-baseline placeholder-gray-300' type= 'text' /> */}
-</div>
-<input onChange={e => setEmail (e.target.value) }  placeholder='Email' className=' bg-slate-600 text-white border-0 rounded-md p-2 mb-4 focus:bg-slate-500
-focus:outline-blue-200 transition ease-in-out duration-150 place-content-baseline placeholder-gray-300' type= 'email' />
-      
-<input onChange={e => setPassword (e.target.value) }  placeholder='Password' className=' bg-slate-600 text-white border-0 rounded-md p-2 mb-4 focus:bg-slate-500
-focus:outline-blue-200 transition ease-in-out duration-150 place-content-baseline placeholder-gray-300' type= 'password' />
-      <button className = 'bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-medium py-2 px-4 rounded-md hover:bg-indigo-600 hover:to-black transition ease-in duration-200' type='submit' > Submit </button> 
-       <p className='text-white mt-4 text-center'  > Already Have an Account? 
-       <a className='text-white-500 hover:underline mt-4 px-1' onClick={() => navigate ('/Login') } > Sign In</a>
-       </p>
-       </form>   
+            </div>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              className=" bg-slate-600 text-white border-0 rounded-md p-2 mb-4 focus:bg-slate-500
+focus:outline-blue-200 transition ease-in-out duration-150 place-content-baseline placeholder-gray-300"
+              type="email"
+            />
 
-{/* 
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              className=" bg-slate-600 text-white border-0 rounded-md p-2 mb-4 focus:bg-slate-500
+focus:outline-blue-200 transition ease-in-out duration-150 place-content-baseline placeholder-gray-300"
+              type="password"
+            />
+            <button
+              className="bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-medium py-2 px-4 rounded-md hover:bg-indigo-600 hover:to-black transition ease-in duration-200"
+              type="submit"
+            >
+              {" "}
+              Submit{" "}
+            </button>
+            <p className="text-white mt-4 text-center">
+              {" "}
+              Already Have an Account?
+              <a
+                className="text-white-500 hover:underline mt-4 px-1"
+                onClick={() => navigate("/Login")}
+              >
+                {" "}
+                Sign In
+              </a>
+            </p>
+          </form>
+
+          {/* 
         <h1  className='font-bold'> Hello </h1> */}
-         </div >
         </div>
-       
-
-        </div>
-  )
-
+      </div>
+    </div>
+  );
 }
 
-export default Signup
+Signup.propTypes = {
+  setToken: PropTypes.func.isRequired,
+};
+
+export default Signup;
