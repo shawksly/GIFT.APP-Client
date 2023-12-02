81999ccfb41337843c764c518e7e010abcab76b8
shawksly <shawksly@gmail.com>
2023-12-02 13:11:15 -0700

    converted css to tailwind for listgroup and list items

--- src/components/listGroup/ListGroup.jsx
+++ src/components/listGroup/ListGroup.jsx
@@ -9,169 +9,47 @@ function ListGroup({ token, updateUser, fetchLists, lists }) {
 
   return (
     <div>
-      <div className='container min-h-screen flex items-center justify-center relative overflow-hidden bg-gray-800'>
-        <div className='box min-w-350 min-h-400 bg-white bg-opacity-25 rounded-2xl z-10 p-8'>
-          <h3 className='mb-8 text-white'>GIFT.LISTS</h3>
+      <div className='m-0 px-0 pb-0 pt-2 box-border font-poppins min-h-screen min-w-[100vw] flex items-center justify-center relative overflow-hidden bg-[#222]'>
+        <div className='m-0 box-border font-poppins min-w-[350px] min-h-[400px] bg-[#ffffff1a] bg-opacity-25 rounded-[10px] z-10 p-5'>
+          <h3 className='mx-0 mt-0 mb-[25px] px-0 pb-0 pt-2 box-border font-poppins text-white'>GIFT.LISTS</h3>
           {lists?.map((list) => {
             return (
               <Fragment key={list._id}>
-                <div className='list'>
-                  <div className='imgBox'></div>
-                  <div className='info'>
-                    <h2>
-                      <small>#</small> 1
+                <div className='mx-0 mb-0 mt-2.5 p=10 box-border font-poppins relative overflow-hidden flex items-center p-2.5 gap-2.5 rounded-[10px] bg-[#0000001a] cursor-pointer transition-all duration-200 ease-linear hover:bg-zinc-100 hover:scale-[1.03] hover:translate-x-[6px] hover:translate-y-[-3px] group/list'>
+                  <div className='m-0 px-0 pb-0 pt-2 box-border font-poppins relative w-[60px] h-[60px] rounded-[10px] overflow-hidden'>
+                    <h5 className="text-3xl text-center font-poppins text-white group-hover/list:text-[#222]">🎁</h5>
+                    {/* <img src="./images/1.jpg" alt="" className='m-0 px-0 pb-0 pt-2 box-border font-poppins object-cover w-full h-full'> */}
+                  </div>
+                  <div className='m-0 px-0 pb-0 pt-2 box-border font-poppins text-white'>
+                    <h2 className='absolute right-[-55px] top-1/2 -translate-y-1/2 text-[#00aeff] duration-200 text-[2em] group-hover/list:right-5'>
+                      <small className='font-medium opacity-25'>#</small> 1
                     </h2>
-                    <h4 className='capitalize'>{list.title}</h4>
-                    <p className='lowercase'>{list.description}</p>
+                    <h4 className='font-semibold leading-[1.2em] duration-200 group-hover/list:text-[#222] capitalize'>{list.title}</h4>
+                    <p className='text-xs duration-200 group-hover/list:text-[#222] lowercase'>{list.description}</p>
+                    <p className='underline absolute bottom-2.5 right-14 mr-4 text-[0.75em] group-hover/list:text-[#222] duration-200'>
+                      Edit
+                    </p>
                   </div>
                 </div>
               </Fragment>
             );
           })}
-          <div className='list'>
-            <div className='imgBox'>
-              {/* <img src="./images/1.jpg" alt=""> */}
-            </div>
-            <div className='info'>
-              <h2>
-                <small>#</small> 1
-              </h2>
-              <h4>Item Title</h4>
-              <p>Item Description</p>
-            </div>
+        <div className='mx-0 mb-0 mt-2.5 p=10 box-border font-poppins relative overflow-hidden flex items-center p-2.5 gap-2.5 rounded-[10px] bg-[#0000001a] cursor-pointer transition-all duration-200 ease-linear hover:bg-zinc-100 hover:scale-[1.03] hover:translate-x-[6px] hover:translate-y-[-3px] group/list'>
+          <div className='m-0 px-0 pb-0 pt-2 box-border font-poppins relative w-[60px] h-[60px] rounded-[10px] overflow-hidden'>
+            <h5 className="text-3xl text-center font-poppins text-white group-hover/list:text-[#222]">G</h5>
+            {/* <img src="./images/1.jpg" alt="" className='m-0 px-0 pb-0 pt-2 box-border font-poppins object-cover w-full h-full'> */}
           </div>
-          <div className='list'>
-            <div className='imgBox'>
-              {/* <img src="./images/1.jpg" alt=""> */}
-            </div>
-            <div className='info'>
-              <h2>
-                <small>#</small> 1
-              </h2>
-              <h4>Item Title</h4>
-              <p>Item Description</p>
-            </div>
+          <div className='m-0 px-0 pb-0 pt-2 box-border font-poppins text-white'>
+            <h2 className='absolute right-[-55px] top-1/2 -translate-y-1/2 text-[#00aeff] duration-200 text-[2em] group-hover/list:right-5'>
+              <small className='font-medium opacity-25'>#</small> 1
+            </h2>
+            <h4 className='font-semibold leading-[1.2em] duration-200 group-hover/list:text-[#222]'>Item Title</h4>
+            <p className='text-xs duration-200 group-hover/list:text-[#222]'>Item Description</p>
+            <p className='underline absolute bottom-2.5 right-14 mr-4 text-[0.75em] group-hover/list:text-[#222] duration-200'>
+              Edit
+            </p>
           </div>
-          <div className='list'>
-            <div className='imgBox'>
-              {/* <img src="./images/1.jpg" alt=""> */}
-            </div>
-            <div className='info'>
-              <h2>
-                <small>#</small> 1
-              </h2>
-              <h4>Item Title</h4>
-              <p>Item Description</p>
-            </div>
-          </div>
-          <div className='list'>
-            <div className='imgBox'>
-              {/* <img src="./images/1.jpg" alt=""> */}
-            </div>
-            <div className='info'>
-              <h2>
-                <small>#</small> 1
-              </h2>
-              <h4>Item Title</h4>
-              <p>Item Description</p>
-            </div>
-          </div>
-          <div className='list'>
-            <div className='imgBox'>
-              {/* <img src="./images/1.jpg" alt=""> */}
-            </div>
-            <div className='info'>
-              <h2>
-                <small>#</small> 1
-              </h2>
-              <h4>Item Title</h4>
-              <p>Item Description</p>
-            </div>
-          </div>
-          <div className='list'>
-            <div className='imgBox'>
-              {/* <img src="./images/1.jpg" alt=""> */}
-            </div>
-            <div className='info'>
-              <h2>
-                <small>#</small> 1
-              </h2>
-              <h4>Item Title</h4>
-              <p>Item Description</p>
-            </div>
-          </div>
-          <div className='list'>
-            <div className='imgBox'>
-              {/* <img src="./images/1.jpg" alt=""> */}
-            </div>
-            <div className='info'>
-              <h2>
-                <small>#</small> 1
-              </h2>
-              <h4>Item Title</h4>
-              <p>Item Description</p>
-            </div>
-          </div>
-          <div className='list'>
-            <div className='imgBox'>
-              {/* <img src="./images/1.jpg" alt=""> */}
-            </div>
-            <div className='info'>
-              <h2>
-                <small>#</small> 1
-              </h2>
-              <h4>Item Title</h4>
-              <p>Item Description</p>
-            </div>
-          </div>
-          <div className='list'>
-            <div className='imgBox'>
-              {/* <img src="./images/1.jpg" alt=""> */}
-            </div>
-            <div className='info'>
-              <h2>
-                <small>#</small> 1
-              </h2>
-              <h4>Item Title</h4>
-              <p>Item Description</p>
-            </div>
-          </div>
-          <div className='list'>
-            <div className='imgBox'>
-              {/* <img src="./images/1.jpg" alt=""> */}
-            </div>
-            <div className='info'>
-              <h2>
-                <small>#</small> 1
-              </h2>
-              <h4>Item Title</h4>
-              <p>Item Description</p>
-            </div>
-          </div>
-          <div className='list'>
-            <div className='imgBox'>
-              {/* <img src="./images/1.jpg" alt=""> */}
-            </div>
-            <div className='info'>
-              <h2>
-                <small>#</small> 1
-              </h2>
-              <h4>Item Title</h4>
-              <p>Item Description</p>
-            </div>
-          </div>
-          <div className='list'>
-            <div className='imgBox'>
-              {/* <img src="./images/1.jpg" alt=""> */}
-            </div>
-            <div className='info'>
-              <h2>
-                <small>#</small> 1
-              </h2>
-              <h4>Item Title</h4>
-              <p>Item Description</p>
-            </div>
-          </div>
+        </div>
         </div>
       </div>
     </div>
