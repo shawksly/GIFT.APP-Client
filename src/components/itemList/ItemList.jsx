4c10fa4337387cca821dd0815e57a26eca15b474
shawksly <shawksly@gmail.com>
2023-12-05 07:08:22 -0700

    fixed merge issues

--- src/components/itemList/ItemList.jsx
+++ src/components/itemList/ItemList.jsx
@@ -70,51 +70,15 @@ function ItemList({ token, setListDisplay, gifts, giftsId, fetchGifts }) {
                   </div>
                 </div>
               ))}
-          <div className='mx-0 mb-0 mt-2.5 p=10 box-border font-poppins relative overflow-hidden flex items-center p-2.5 gap-2.5 rounded-[10px] bg-[#0000001a] cursor-pointer transition-all duration-200 ease-linear hover:bg-zinc-100 hover:scale-[1.03] hover:translate-x-[6px] hover:translate-y-[-3px] group/list'>
-            <div className='m-0 px-0 pb-0 pt-2 box-border font-poppins relative w-[60px] h-[20px] rounded-[10px] overflow-hidden'>
-              {/* <img src="./images/1.jpg" alt="" className='m-0 px-0 pb-0 pt-2 box-border font-poppins object-cover w-full h-full'> */}
-            </div>
-            <div className='m-0 px-0 pb-0 pt-2 box-border font-poppins text-white'>
-              <h2 className='absolute right-[-55px] top-1/2 -translate-y-1/2 text-[#00aeff] duration-200 text-[2em] group-hover/list:right-5'>
-                <small className='font-medium opacity-25'>#</small> 1
-              </h2>
-              <h4 className='font-semibold leading-[1.2em] duration-200 group-hover/list:text-[#222]'>
-                Item Title
-              </h4>
-              <p className='text-xs duration-200 group-hover/list:text-[#222]'>
-                Item Description
-              </p>
-              <p className='underline absolute bottom-2.5 right-14 mr-4 text-[0.75em] group-hover/list:text-[#222] duration-200'>
-                Edit
-              </p>
-            </div>
-          </div>
         </div>
       </div>
     </motion.div>
-{/*********************************************************/}
-    <div>
-      <div className='m-0 px-0 pb-0 pt-2 box-border font-poppins min-h-screen min-w-[100vw] flex items-center justify-center relative overflow-hidden bg-[#222]'>
-  <div className='m-0 box-border font-poppins min-w-[350px] min-h-[200px] bg-[#ffffff1a] bg-opacity-25 rounded-[10px] z-10 p-5'>
-    <h3 className='mx-0 mt-0 mb-[25px] px-0 pb-0 pt-2 box-border font-poppins text-white'>ITEM.LISTS</h3>
+  );
+}
 
-    <div className='mx-0 mb-0 mt-2.5 p=10 box-border font-poppins relative overflow-hidden flex items-center p-2.5 rounded-[10px] bg-[#0000001a] cursor-pointer transition-all duration-200 ease-linear hover:bg-zinc-100 hover:scale-[1.03] hover:translate-x-[6px] hover:translate-y-[-3px] group/list'>
-      <div className='m-0 px-0 pb-0 pt-2 box-border font-poppins relative w-[60px] h-[20px] rounded-[10px] overflow-hidden'>
-        {/* <img src="./images/1.jpg" alt="" className='m-0 px-0 pb-0 pt-2 box-border font-poppins object-cover w-full h-full'> */}
-      </div>
-      <div className='m-0 px-0 pb-0 pt-2 box-border font-poppins text-white'>
-        <h2 className='absolute right-[-55px] top-1/2 -translate-y-1/2 text-[#00aeff] duration-200 text-[2em] group-hover/list:right-5'>
-          <small className='font-medium opacity-25'>#</small> 1
-        </h2>
-        <h4 className='font-semibold leading-[1.2em] duration-200 group-hover/list:text-[#222] capitalize'>Item Title</h4>
-        <p className='text-xs duration-200 group-hover/list:text-[#222] lowercase'>Item Description</p>
-        <p className='underline absolute bottom-2.5 right-14 mr-4 text-[0.75em] group-hover/list:text-[#222] duration-200'>
-          Edit
-        </p>
-      </div>
-    </div>
+export default ItemList;
 
-<div className="w-full max-w-md p-4 bg-[#ffffff1a] bg-opacity-25 rounded border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+{/* <div className="w-full max-w-md p-4 bg-[#ffffff1a] bg-opacity-25 rounded border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
     <div className="flex items-center justify-between mb-4">
         <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Latest Customers</h5>
         <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
@@ -215,28 +179,4 @@ function ItemList({ token, setListDisplay, gifts, giftsId, fetchGifts }) {
             </li>
         </ul>
    </div>
-</div>
-
-
-    <div className='mx-0 mb-0 mt-2.5 p=10 box-border font-poppins relative overflow-hidden flex items-center p-2.5 rounded-[10px] bg-[#0000001a] cursor-pointer transition-all duration-200 ease-linear hover:bg-zinc-100 hover:scale-[1.03] hover:translate-x-[6px] hover:translate-y-[-3px] group/list'>
-      <div className='m-0 px-0 pb-0 pt-2 box-border font-poppins relative w-[60px] h-[20px] rounded-[10px] overflow-hidden'>
-        {/* <img src="./images/1.jpg" alt="" className='m-0 px-0 pb-0 pt-2 box-border font-poppins object-cover w-full h-full'> */}
-      </div>
-      <div className='m-0 px-0 pb-0 pt-2 box-border font-poppins text-white'>
-        <h2 className='absolute right-[-55px] top-1/2 -translate-y-1/2 text-[#00aeff] duration-200 text-[2em] group-hover/list:right-5'>
-          <small className='font-medium opacity-25'>#</small> 1
-        </h2>
-        <h4 className='font-semibold leading-[1.2em] duration-200 group-hover/list:text-[#222]'>Item Title</h4>
-        <p className='text-xs duration-200 group-hover/list:text-[#222]'>Item Description</p>
-        <p className='underline absolute bottom-2.5 right-14 mr-4 text-[0.75em] group-hover/list:text-[#222] duration-200'>
-          Edit
-        </p>
-      </div>
-    </div>
-    </div>
-    </div>
-    </div>
-  );
-}
-
-export default ItemList;
+</div> */}
--- src/components/listGroup/ListGroup.jsx
+++ src/components/listGroup/ListGroup.jsx
@@ -34,7 +34,9 @@ function ListGroup({ token, setListDisplay, fetchLists, lists, setgiftsId }) {
           <h3 className='mx-0 mt-0 mb-[25px] px-0 pb-0 pt-2 box-border font-poppins text-white'>
             GIFT.LISTS
           </h3>
-          {lists?.map((list) => {
+          {!lists.listOwner
+            ? null
+            : lists.listOwner?.map((list) => {
             return (
               <Fragment key={list._id}>
                 <div className='mx-0 mb-0 mt-2.5 p=10 box-border font-poppins relative overflow-hidden p-2.5 gap-2.5 rounded-[10px] bg-[#0000001a] cursor-pointer transition-all duration-200 ease-linear hover:bg-zinc-100 hover:scale-[1.03] hover:translate-x-[6px] hover:translate-y-[-3px] group/list'>
@@ -78,37 +80,6 @@ function ListGroup({ token, setListDisplay, fetchLists, lists, setgiftsId }) {
               </Fragment>
             );
           })}
-
-          <div className='mx-0 mb-0 mt-2.5 p=10 box-border font-poppins relative overflow-hidden flex items-center p-2.5 gap-2.5 rounded-[10px] bg-[#0000001a] cursor-pointer transition-all duration-200 ease-linear hover:bg-zinc-100 hover:scale-[1.03] hover:translate-x-[6px] hover:translate-y-[-3px] group/list'>
-            <div className='m-0 px-0 pb-0 pt-2 box-border font-poppins relative w-[60px] h-[60px] rounded-[10px] overflow-hidden'>
-              <h5 className='text-3xl text-center font-poppins text-white group-hover/list:text-[#222]'>
-                G
-              </h5>
-              {/* <img src="./images/1.jpg" alt="" className='m-0 px-0 pb-0 pt-2 box-border font-poppins object-cover w-full h-full'> */}
-            </div>
-            <div className='m-0 px-0 pb-0 pt-2 box-border font-poppins text-white'>
-              <h2 className='absolute right-[-55px] top-1/2 -translate-y-1/2 text-[#00aeff] duration-200 text-[2em] group-hover/list:right-5'>
-                <small className='font-medium opacity-25'>#</small> 1
-              </h2>
-              <h4 className='font-semibold leading-[1.2em] duration-200 group-hover/list:text-[#222]'>
-                Item Title
-              </h4>
-              <p className='text-xs duration-200 group-hover/list:text-[#222]'>
-                Item Description
-              </p>
-              <div className='absolute bottom-0 right-0 flex flex-col w-14 h-20'>
-                <button
-                  onClick={() => navigate('/ItemList')}
-                  className='w-14 bg-blue-500 rounded-5 grow'
-                >
-                  Edit
-                </button>
-                <button className='w-14 bg-red-500 rounded-5 grow'>
-                  Delete
-                </button>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </motion.div>
